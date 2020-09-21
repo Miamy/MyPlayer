eeacc23d41eb3da40c03366b7aac1067831e279d
Miamy <miamy@ukr.net>
2020-09-21 18:37:52 +0300

    2

--- MyPlayer/ViewModels/QueueViewModel.cs
+++ MyPlayer/ViewModels/QueueViewModel.cs
@@ -14,20 +14,16 @@ namespace MyPlayer.ViewModels
     {
         public IQueue Queue { get; set; }
 
-        public List<SongModel> Songs => Queue.Songs.Select(song => new SongModel(Queue.Songs.IndexOf(song) + 1, song)).ToList();
+        //public List<SongModel> Songs => Queue.Songs.Select(song => new SongModel(Queue.Songs.IndexOf(song) + 1, song)).ToList();
         //public List<ISong> Songs => Queue.Songs.ToList();
 
-        public List<AlbumModel> Albums => /*new List<IAlbum>()
-        {
-            new Album("1"),
-            new Album("2"),
-            new Album("3"),
-        };*/
-            //Queue.Albums.ToList();
-            Queue.Albums.Select(album => new AlbumModel(album.Name, album.Songs)).ToList();
+        //public List<SongModel> Songs => Queue.Songs.GroupBy(song => song.Album.Artist).GroupBy(song => song.Key.Albums).Select(song => new SongModel(Queue.Songs.IndexOf(song.) + 1, song)).ToList();
+        public List<IGrouping<IArtist, IGrouping<IAlbum, ISong>>> Songs => Queue.Songs.GroupBy(song => song.Album).GroupBy(song => song.Key.Artist).ToList();
+
+        //public List<AlbumModel> Albums => Queue.Albums.Select(album => new AlbumModel(album.Name, album.Songs)).ToList();
 
         //public List<IArtist> Artists => Queue.Artists.ToList();
-        public List<ArtistModel> Artists => Queue.Artists.Select(artist => new ArtistModel(artist.Name, artist.Albums)).ToList();
+        //public List<ArtistModel> Artists => Queue.Artists.Select(artist => new ArtistModel(artist.Name, artist.Albums)).ToList();
 
 
         public ICommand AddToQueueCommand { get; set; }
