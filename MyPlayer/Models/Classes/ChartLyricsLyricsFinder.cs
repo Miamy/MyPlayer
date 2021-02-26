c80c1c133c88d110c4e6dcce83ecb17697d183aa
Miamy <miamy@ukr.net>
2021-02-26 18:46:07 +0300

    64

---
+++ MyPlayer/Models/Classes/ChartLyricsLyricsFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MyPlayer.Models.Interfaces;
+
+namespace MyPlayer.Models.Classes
+{
+    public class ChartLyricsLyricsFinder : ILyricsFinder
+    {
+        public StringBuilder FindLyrics(string artist, string song)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<StringBuilder> FindLyricsAsync(string artist, string song)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyPlayer/Models/Interfaces/ILyricsFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlayer.Models.Interfaces
+{
+    public interface ILyricsFinder
+    {
+        StringBuilder FindLyrics(string artist, string song);
+        Task<StringBuilder> FindLyricsAsync(string artist, string song)
+    }
+}
