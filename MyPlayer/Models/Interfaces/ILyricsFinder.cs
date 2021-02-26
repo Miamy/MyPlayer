2203b920fccbfedff52c5cfee5247fc9c32e7413
Miamy <miamy@ukr.net>
2021-02-26 18:50:26 +0300

    65

--- MyPlayer/Models/Interfaces/ILyricsFinder.cs
+++ MyPlayer/Models/Interfaces/ILyricsFinder.cs
@@ -8,6 +8,6 @@ namespace MyPlayer.Models.Interfaces
     public interface ILyricsFinder
     {
         StringBuilder FindLyrics(string artist, string song);
-        Task<StringBuilder> FindLyricsAsync(string artist, string song)
+        Task<StringBuilder> FindLyricsAsync(string artist, string song);
     }
 }
